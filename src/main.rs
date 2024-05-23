554fd56f29ea89278fadb1f1edd2650f5f6e27cb
ariaandika <andikaakun17@gmail.com>
2024-05-23 11:58:33 +0700

    add: axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[tokio::main]
+async fn main() {
+
+    let router = routes();
+
+    let tcp = tokio::net::TcpListener::bind("127.0.0.1:3000").await.expect("unable to bind port 3000");
+
+    axum::serve(tcp, router).await.expect("unable to start server");
 }
+
+fn routes() -> axum::Router {
+    axum::Router::new()
+}
+
